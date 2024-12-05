e5c49624cf8463a67a983e11e27533dc68ef79b5
Liudmila Khamiakova <lv.khomyakova@gmail.com>
2024-12-05 14:08:37 +0300

    fix: hw-8 PR fixes

--- src/components/pages/restaurant-page/RestaurantPage.jsx
+++ src/components/pages/restaurant-page/RestaurantPage.jsx
@@ -1,15 +1,8 @@
 import { RestaurantContainer } from "../../restaurant/Restaurant-container.jsx";
-import { Navigate, useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectRestaurantsIds } from "../../../redux/entities/restaurants/restaurants-slice.js";
+import { useParams } from "react-router-dom";
 
 export const RestaurantPage = () => {
   const {restaurantId} = useParams();
-  const restaurantIds = useSelector(selectRestaurantsIds);
-
-  if (!restaurantId && restaurantIds.length > 0) {
-    return <Navigate to={`/restaurant/${restaurantIds[0]}/menu`} />;
-  }
 
   return <RestaurantContainer id={restaurantId}/>;
 }
